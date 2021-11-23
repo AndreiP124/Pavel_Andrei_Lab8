43e367400ffd9e56756cdd88ba356b0d0bd92a32
ANDREI PAVEL <andrei.pavel2@stud.ubbcluj.ro>
2021-11-23 17:02:04 +0200

    Lab8

---
+++ Pavel_Andrei_Lab8/Data/Pavel_Andrei_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pavel_Andrei_Lab8.Models;
+
+namespace Pavel_Andrei_Lab8.Data
+{
+    public class Pavel_Andrei_Lab8Context : DbContext
+    {
+        public Pavel_Andrei_Lab8Context (DbContextOptions<Pavel_Andrei_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pavel_Andrei_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Pavel_Andrei_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Pavel_Andrei_Lab8.Models.Category> Category { get; set; }
+    }
+}
